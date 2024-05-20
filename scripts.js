5912829ebf790e51763146d30f0fd7f48246846a
Sasha Isler <67922461+sisler101@users.noreply.github.com>
2024-05-20 19:14:44 -0400

    initial framework for index scripts and styles

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Function to open a modal
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+
+    // When the user clicks anywhere outside of the modal content, close it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
+
+    // When the user presses the Esc key, close it
+    window.onkeydown = function(event) {
+        if (event.key === 'Escape') {
+            modal.style.display = 'none';
+        }
+    }
+}
+
+// Function to close a modal
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
